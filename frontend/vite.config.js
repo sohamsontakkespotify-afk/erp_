d04cee4e7eacea6ed7199cff0bf946dddbbdcae5
sohamsontakkespotify-afk <sohamsontakke.spotify@gmail.com>
2025-10-16 09:46:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "erp-3p2p.vercel.app",
         changeOrigin: true,
         secure: false,
       },
